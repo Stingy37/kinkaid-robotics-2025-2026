9d91cc9ceeddce0284f8dd711fc1c8a88090ad81
andyshi <as.andy.shi@gmail.com>
2025-11-17 16:54:02 -0600

    started UI work

--- src/main.py
+++ src/main.py
@@ -2,27 +2,62 @@
 Actual program that runs on the vex brain
 """
 # Library imports
+import time
 from vex import *
-from robot_config import *
+from robot_config import * # imports all the hardware definitions for the robot
+
 
 def driver_control():
     """
-    Function that runs when the user is controlling the robot 
+    Code that runs when the user is controlling the robot 
     """
     # called every 5 millisecs to not fry CPU but keep robot responsive to users inputs 
     #   - while loop listens to users input
     while True:
-        left_motor.set_velocity((controller_1.axis3.position() + controller_1.axis1.position()), PERCENT)
-        right_motor.set_velocity((controller_1.axis3.position() - controller_1.axis1.position()), PERCENT)
-        left_motor.spin(FORWARD)
-        right_motor.spin(FORWARD)
-        wait(5, MSEC)
+        pass
+
 
 def autonomous():
-    pass
+    """
+    Code that runs when the robot is in auton mode
+    """
+    # --- initial calibration
+    gyro.calibrate() 
+    dt.set_drive_velocity(50, PERCENT)
+    dt.set_turn_velocity(50, PERCENT)
+
+
+    # ---- move robot 
+    intake_motorgroup.set_velocity(25, PERCENT)
+    intake_motorgroup.spin(FORWARD)
+    time.sleep(.25)
+
+    intake_motorgroup.set_velocity(100, PERCENT)
+    intake_motorgroup.spin(REVERSE)
+
+    # ---- pick up items
+    flywheel.set_velocity(50, PERCENT)
+    conveyor.spin(REVERSE)
+    flywheel.spin(FORWARD)
+
+# delegates robot behavior during competition
+competition = Competition(autonomous, driver_control)
 
 def main():
-    autonomous()
-    driver_control()
+    """
+    sets up and updates the UI
+    """
+    brain.screen.clear_screen()
+
+    # set the text color 
+    brain.screen.set_pen_color(Color.WHITE)
+
+    # top right of the screen (where rows and columns are 1-index, NOT zero-indexed!)
+    brain.screen.set_cursor(1, 1)
+    brain.screen.print("you suck lucas")
+
+    while True:
+        # update UI
+        wait(20, MSEC) 
 
 main()
--- todo.md
+++ todo.md
@@ -1,5 +1,5 @@
 #### TODOS: 
 - create a motor group for ports 3 and 4 (make sure they spin)
-- convert the current drivetrain into a 4 motor drivetrain
+- convert the current dt into a 4 motor dt
 - limit drive speed forward backwart to 70% max speed
 - limit turn speed to only 50% of max speed
